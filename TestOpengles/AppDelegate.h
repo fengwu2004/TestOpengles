162f5f1f7a8a579ea2444377100f94a336186bc3
yl <yanli@yellfun.com>
2016-07-20 16:15:40 +0800

    Initial Commit

---
+++ TestOpengles/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestOpengles
+//
+//  Created by ky on 16/7/20.
+//  Copyright © 2016年 yellfun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestOpengles/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestOpengles
+//
+//  Created by ky on 16/7/20.
+//  Copyright © 2016年 yellfun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
